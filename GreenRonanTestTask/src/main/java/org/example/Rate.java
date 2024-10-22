83cacdb0e60b4cdd6fd0f28320ad7a0a0dd16fba
RonanGreen1 <ronangreen2003@gmail.com>
2024-10-22 09:53:41 +0100

    added check for hours to rate class

--- GreenRonanTestTask/src/main/java/org/example/Rate.java
+++ GreenRonanTestTask/src/main/java/org/example/Rate.java
@@ -9,6 +9,17 @@ public class Rate {
     private final BigDecimal hourlyNormalRate;
     private final BigDecimal hourlyReducedRate;
 
+    Public Rate(CarParkKind Kind, BigDecimal hourlyNormalRate, BigDecimal hourlyReducedRate){
+        if (hourlyNormalRate.compareTo(BigDecimal.ZERO) < 0 || 
+            hourlyNormalRate.compareTo(BigDecimal.TEN) > 0 || 
+            hourlyReducedRate.compareTo(BigDecimal.ZERO) < 0 || 
+            hourlyReducedRate.compareTo(BigDecimal.TEN) > 0 || 
+            hourlyNormalRate.compareTo(hourlyReducedRate) < 0) {
+            throw new IllegalArgumentException("Hourly rates must be within valid ranges and normal rate must be greater than or equal to reduced rate");
+        }
+        
+    }
+
 }
 
 enum CarParkKind {
