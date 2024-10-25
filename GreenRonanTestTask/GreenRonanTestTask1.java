d4e87d9f1e2e9d1a1486e20ccba9d98d85ad9c6e
RonanGreen1 <ronangreen2003@gmail.com>
2024-10-25 12:54:11 +0100

    finished format

---
+++ GreenRonanTestTask/GreenRonanTestTask1.java
@@ -0,0 +1,9 @@
+package org.example;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({GreenRonanRateTest.class, GreenRonanPeriodTest.class})
+public class GreenRonanTestTask1 {
+}
